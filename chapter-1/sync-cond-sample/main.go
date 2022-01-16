addef621b4400fc8befa4ae0572457432a47f592
daku10 <daku.two@gmail.com>
2022-01-16 14:43:01 +0900

    add another sync sample

--- chapter-1/sync-cond-sample/main.go
+++ chapter-1/sync-cond-sample/main.go
@@ -8,11 +8,16 @@ import (
 
 func main() {
 
+	var o sync.Once
+
 	m := new(sync.Mutex)
 	c := sync.NewCond(m)
 
 	for i := 0; i < 10; i++ {
 		go func(i int) {
+			o.Do(func() {
+				fmt.Println("once!")
+			})
 			fmt.Printf("wait %d\n", i)
 			c.L.Lock()
 			defer c.L.Unlock()
@@ -35,4 +40,31 @@ func main() {
 	time.Sleep(1 * time.Second)
 	c.Broadcast()
 	time.Sleep(3 * time.Second)
+
+	p := sync.Pool{
+		New: func() interface{} {
+			return "定時作業"
+		},
+	}
+	
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 10; i++ {
+			p.Put("割込作業")
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 10; i++ {
+			fmt.Println(p.Get())
+			time.Sleep(500 * time.Millisecond)
+		}
+	}()
+
+	wg.Wait()
 }
