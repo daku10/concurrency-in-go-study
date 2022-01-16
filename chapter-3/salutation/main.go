d9c94c82d4660766edfd2fdafe9f98c76e28d645
daku10 <daku.two@gmail.com>
2022-01-16 19:52:14 +0900

    use channel

--- chapter-3/salutation/main.go
+++ chapter-3/salutation/main.go
@@ -9,12 +9,17 @@ func main() {
 
 	salutation := []string{"hello", "greetings", "good day"}
 	var wg sync.WaitGroup
+	ch := make(chan string)
 	for _, word := range salutation {
 		wg.Add(1)
-		go func(word string) {
-			fmt.Println(word)
+		// 先に入れると受信されるまでブロックされる、かつ受信するgoroutineはこの後の行なので、deadlockと判定される
+		// ch <- word
+		go func() {
+			word2 := <-ch
+			fmt.Println(word2)
 			wg.Done()
-		}(word)		
+		}()		
+		ch <- word
 	}
 	wg.Wait()
 }
