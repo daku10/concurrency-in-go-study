af014e896da89a26373cc0e80c1b4382cabacaea
daku10 <daku.two@gmail.com>
2022-01-16 20:05:29 +0900

    measure consumed-memory of goroutine

---
+++ chapter-3/consumed-memory-measurement/go.mod
@@ -0,0 +1,3 @@
+module github.com/daku10/concurrency-in-go-study/chapter-3/consumed-memory-measurement
+
+go 1.17
---
+++ chapter-3/consumed-memory-measurement/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main() {
+	memConsumed := func() uint64 {
+		runtime.GC()
+		var s runtime.MemStats
+		runtime.ReadMemStats(&s)
+		return s.Sys
+	}
+
+	var c <-chan interface{}
+	var wg sync.WaitGroup
+	noop := func() {wg.Done(); <-c}
+
+	const numGorutines = 1e4
+	wg.Add(numGorutines)
+	before := memConsumed()
+	for i := numGorutines; i > 0; i-- {
+		go noop()
+	}
+	wg.Wait()
+	after := memConsumed()
+	fmt.Printf("%.3fkb", float64(after - before) / numGorutines / 1000)
+}
