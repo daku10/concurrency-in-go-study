b216a7734a2f34e50640ec5d34170582360d25da
daku10 <daku.two@gmail.com>
2022-01-19 17:26:51 +0900

    add example of context value of owend type

--- chapter-4/context-value/main.go
+++ chapter-4/context-value/main.go
@@ -5,16 +5,31 @@ import (
 	"fmt"
 )
 
+type ctxKey int
+
+const (
+	ctxUserID ctxKey = iota
+	ctxAuthToken
+)
+
+func UserID(c context.Context) string {
+	return c.Value(ctxUserID).(string)
+}
+
+func AuthToken(c context.Context) string {
+	return c.Value(ctxAuthToken).(string)
+}
+
 func main() {
 	ProcessRequest("jane", "abc123")
 }
 
 func ProcessRequest(userID, authToken string) {
-	ctx := context.WithValue(context.Background(), "userID", userID)
-	ctx = context.WithValue(ctx, "authToken", authToken)
+	ctx := context.WithValue(context.Background(), ctxUserID, userID)
+	ctx = context.WithValue(ctx, ctxAuthToken, authToken)
 	HandleResponse(ctx)
 }
 
 func HandleResponse(ctx context.Context) {
-	fmt.Printf("handling response for %v (%v)", ctx.Value("userID"), ctx.Value("authToken"))
+	fmt.Printf("handling response for %v (%v)", UserID(ctx), AuthToken(ctx))
 }
