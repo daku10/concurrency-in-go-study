586ebb9898873d2dbb7711ed421feec60821750b
daku10 <daku.two@gmail.com>
2022-01-22 22:20:28 +0900

    add several multilimiter

--- chapter-5/rate-limit/main.go
+++ chapter-5/rate-limit/main.go
@@ -46,30 +46,37 @@ func Per(eventCount int, duration time.Duration) rate.Limit {
 }
 
 func Open() *APIConnection {
-	secondLimit := rate.NewLimiter(Per(2, time.Second), 1)
-	minuteLimit := rate.NewLimiter(Per(10, time.Minute), 10)
 	return &APIConnection{
-		rateLimiter: MultiLimiter(secondLimit, minuteLimit),
+		networkLimit: MultiLimiter(
+			rate.NewLimiter(Per(2, time.Second), 2),
+			rate.NewLimiter(Per(10, time.Minute), 10),
+		),
+		diskLimit: MultiLimiter(
+			rate.NewLimiter(rate.Limit(1), 1),
+		),
+		apiLimit: MultiLimiter(
+			rate.NewLimiter(Per(3, time.Second), 3),
+		),
 	}
 }
 
 type APIConnection struct {
-	rateLimiter RateLimiter
+	networkLimit, diskLimit, apiLimit RateLimiter
 }
 
 func (a *APIConnection) ReadFile(ctx context.Context) error {
-	if err := a.rateLimiter.Wait(ctx); err != nil {
+	err := MultiLimiter(a.apiLimit, a.diskLimit).Wait(ctx)
+	if err != nil {
 		return err
 	}
-	// なにか処理をすると想定
 	return nil
 }
 
 func (a *APIConnection) ResolveAddress(ctx context.Context) error {
-	if err := a.rateLimiter.Wait(ctx); err != nil {
+	err := MultiLimiter(a.apiLimit, a.networkLimit).Wait(ctx)
+	if err != nil {
 		return err
 	}
-	// なにか処理をすると想定
 	return nil
 }
 
