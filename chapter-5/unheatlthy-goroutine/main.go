5f92312299d4755a4c07bf202cc210809f613ffb
daku10 <daku.two@gmail.com>
2022-01-24 20:43:29 +0900

    add working steward example

--- chapter-5/unheatlthy-goroutine/main.go
+++ chapter-5/unheatlthy-goroutine/main.go
@@ -2,10 +2,23 @@ package main
 
 import (
 	"log"
+	"os"
 	"time"
 )
 
 type startGoroutineFn func(done <-chan interface{}, pulseInterval time.Duration) (heartbeat <-chan interface{})
+
+func or(ch1, ch2 <-chan interface{}) <-chan interface{} {
+	ch := make(chan interface{})
+	go func() {
+		defer close(ch)
+		select {
+		case <-ch1:
+		case <-ch2:
+		}
+	}()
+	return ch
+}
 
 func main() {
 	newSteward := func(timeout time.Duration, startGoroutine startGoroutineFn) startGoroutineFn {
@@ -48,4 +61,26 @@ func main() {
 			return heartbeat
 		}
 	}
+
+	log.SetOutput(os.Stdout)
+	log.SetFlags(log.Ltime | log.LUTC)
+
+	doWork := func(done <-chan interface{}, _ time.Duration) <-chan interface{} {
+		log.Println("ward: Hello, I'm ireresponsible!")
+		go func() {
+			<-done
+			log.Println("ward: I am halting.")
+		}()
+		return nil
+	}
+	doWorkWithSteward := newSteward(4 * time.Second, doWork)
+
+	done := make(chan interface{})
+	time.AfterFunc(9 * time.Second, func() {
+		log.Println("main: halting steward and ward.")
+		close(done)
+	})
+
+	for range doWorkWithSteward(done, 4 * time.Second) {}
+	log.Println("Done")
 }
